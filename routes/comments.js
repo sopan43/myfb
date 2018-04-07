a4d775be6b19caa4811f9fe74e12a3ed6a65524e
Sopan Mittal <sopanmittal43@gmail.com>
2018-04-07 22:46:44 +0530

    v10 added edit comment

--- routes/comments.js
+++ routes/comments.js
@@ -37,6 +37,34 @@ router.post('/', isLoggedIn, (req, res) => {
     });
 });
 
+
+//Edit comment
+router.get('/:comment_id/edit', (req, res)=>{
+    Comment.findById(req.params.comment_id, (err, foundComment)=>{
+        if(err){
+            res.redirect('back');
+        }
+        else{
+             res.render('comments/edit', {campground_id: req.params.id, comment: foundComment});
+        }
+    });
+   
+});
+
+//Update Comment
+router.put('/:comment_id', (req, res)=>{
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, (err, foundComment)=>{
+        if(err){
+            res.redirect('back');
+        }
+        else{
+             res.redirect('/campgrounds/'+ req.params.id);
+        }
+    });
+   
+});
+
+//Middleware
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
         return next();
